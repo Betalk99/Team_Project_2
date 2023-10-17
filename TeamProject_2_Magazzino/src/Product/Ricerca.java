ee1bbee70a0213afee60a2d56499f226a2084fe6
Antonio <antoniodat93@gmail.com>
2023-10-17 11:40:04 +0200

    correzione classe Ricerca

--- TeamProject_2_Magazzino/src/Product/Ricerca.java
+++ TeamProject_2_Magazzino/src/Product/Ricerca.java
@@ -1,8 +1,4 @@
 package Product;
 
-public class Ricerca extends Product{
-
-    public Ricerca(TipoProdotti tipo, String producer, String model, String description, double displaySize, double storageCap, double purchPrice, double sellPrice, String itemId) {
-        super(tipo, producer, model, description, displaySize, storageCap, purchPrice, sellPrice, itemId);
-    }
+public class Ricerca {
 }
