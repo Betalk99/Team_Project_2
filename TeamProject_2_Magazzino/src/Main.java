6caa07db8cfed7b7298690fdbf79d5ac6a22d2cb
Bruno Orlandi <40028887+Betalk99@users.noreply.github.com>
2023-10-27 16:37:42 +0200

    Revert "Feature buonanno language standardization"

--- TeamProject_2_Magazzino/src/Main.java
+++ TeamProject_2_Magazzino/src/Main.java
@@ -9,38 +9,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-        ArrayList<Product> stock = new ArrayList<>();  // arraylist magazzino
-        Stock mag = new Stock(stock);
-        ProductBase.baseProd(stock); //richiamare oggetti precaricati arraylist magazzino
-        softwareUsageSelection(stock); // richiamo funzione per accedere alla scelta
+        ArrayList<Product> magazzino = new ArrayList<>();  // arraylist magazzino
+        Stock mag = new Stock(magazzino);
+        ProductBase.baseProd(magazzino); //richiamare oggetti precaricati arraylist magazzino
         ArrayList<Product> carrello = new ArrayList<>();
 
->>>>>>> Develhope
+
+
+        usoGestionaleScelta(magazzino); // richiamo funzione per accedere alla scelta
 
     }
 
-    public static void softwareUsageSelection(ArrayList<Product> stock) throws InputMismatchException {
+    public static void usoGestionaleScelta(ArrayList<Product> magazzino)throws InputMismatchException{
         boolean isTrue = false;
-        while (!isTrue) {
+        while (isTrue == false) {
             try {
                 Scanner in = new Scanner(System.in);
-                System.out.println("Hello, who is going to use this management system? \n1-Customer \n2-Company user");
+                System.out.println("Salve, chi desidera utilizzare questo gestionale: \n1-Utente \n2-Azienda ");
                 int a = in.nextInt();
-                if (a == 1) {
-                    whichOperationCustomer.oper(stock);
-                } else if (a == 2) {
-                    whichOperationCompany.oper(stock);
+                if(a == 1){
+                    whichOperationUser.oper(magazzino);
+                }else if(a == 2){
+                    whichOperationAgency.oper(magazzino);
                 }
-                System.out.println("Would you like to keep searching? 1)Yes   2)No");
+                System.out.println("Vuoi effettuare altra ricerca? 1-Si - 2-No");
                 int stay = in.nextInt();
-                if (stay == 2) {
+                if(stay == 2){
                     isTrue = true;
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Please use a number between 1 (Yes) or 2 (No)");
+                System.out.println("Utilizza un numero tra 1, 2");
                 isTrue = false;
             }
+
         }
+
     }
+
+
 }
