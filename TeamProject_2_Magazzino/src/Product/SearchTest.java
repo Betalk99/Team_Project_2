cc23164f89f63c3f2e6d998bbabc7857535f3b2e
Antonio <antoniodat93@gmail.com>
2023-11-07 12:30:02 +0100

    LAT2zGfU/8-creazione-di-un-metodo-che-permetta-di-fare-la-ricerca-per-tipo-di-dispositivo

--- TeamProject_2_Magazzino/src/Product/SearchTest.java
+++ TeamProject_2_Magazzino/src/Product/SearchTest.java
@@ -12,7 +12,6 @@ class SearchTest {
 
     @Test
     void byCostPriceRangeFrom1To150() {
-
         Stock stock = new Stock();
         stock.setListaProdotti(ProductBase.baseProd(stock.getListaProdotti()));
         double[] rangeValues = {1, 150};
@@ -20,6 +19,59 @@ class SearchTest {
         ArrayList<Product> a = Search.byCostPriceRange(stock, rangeValues);
 
         assertEquals(3, a.size());
+    }
 
+    @Test
+    void getNotebookType() {
+        Stock stock = new Stock();
+        stock.setListaProdotti(ProductBase.baseProd(stock.getListaProdotti()));
+        ArrayList<Product> note = Search.getNotebookType(stock);
+
+        assertEquals(3,note.size());
+    }
+
+    @Test
+    void getNotebookTypeNull() {
+        Stock stock = new Stock();
+        stock.setListaProdotti(ProductBase.baseProd(stock.getListaProdotti()));
+        ArrayList<Product> note = Search.getNotebookType(null);
+
+        assertEquals(null,note);
+    }
+
+    @Test
+    void getTabletType() {
+        Stock stock = new Stock();
+        stock.setListaProdotti(ProductBase.baseProd(stock.getListaProdotti()));
+        ArrayList<Product> tab = Search.getTabletType(stock);
+
+        assertEquals(3,tab.size());
+    }
+
+    @Test
+    void getTabletTypeNull() {
+        Stock stock = new Stock();
+        stock.setListaProdotti(ProductBase.baseProd(stock.getListaProdotti()));
+        ArrayList<Product> tab = Search.getTabletType(null);
+
+        assertEquals(null,tab);
+    }
+
+    @Test
+    void getSmartphoneType() {
+        Stock stock = new Stock();
+        stock.setListaProdotti(ProductBase.baseProd(stock.getListaProdotti()));
+        ArrayList<Product> smartphone = Search.getSmartphoneType(stock);
+
+        assertEquals(3,smartphone.size());
+    }
+
+    @Test
+    void getSmartphoneTypeNull() {
+        Stock stock = new Stock();
+        stock.setListaProdotti(ProductBase.baseProd(stock.getListaProdotti()));
+        ArrayList<Product> smartphone = Search.getSmartphoneType(null);
+
+        assertEquals(null,smartphone);
     }
 }
