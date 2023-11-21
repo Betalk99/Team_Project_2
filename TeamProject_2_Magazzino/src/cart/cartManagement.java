00468574b2c97b2aee95b260a0c7d25ebbbf0855
Luca Di Grigoli <lucadgr92@gmail.com>
2023-11-21 22:29:41 +0100

    c/N0gsgjBY/40

--- TeamProject_2_Magazzino/src/cart/cartManagement.java
+++ TeamProject_2_Magazzino/src/cart/cartManagement.java
@@ -19,49 +19,63 @@ public class cartManagement {
         Scanner sc = new Scanner(System.in);
 //        System.out.println(stock);
         boolean stay = true;
+        boolean stay3 = true;
         while (stay) {
-            System.out.println("""
-                    Hello dear customer, please select one of the following options:
-                     1) Cart status\s
-                     2) Add product/s to cart via ID\s
-                     3) Remove product/s to cart via ID\s
-                     4) Proceed to checkout\s
-                     5) Add products to your cart\s
-                     6) Get the total price of the items in the cart\s
-                     7) Get empty cart
-                     8) Get the average amount spent""");
+            while (stay3) {
+                System.out.println("""
+                        Hello dear customer, please select one of the following options:
+                         1) Cart status\s
+                         2) Add product/s to cart via ID\s
+                         3) Remove product/s to cart via ID\s
+                         4) Proceed to checkout\s
+                         5) Add products to your cart\s
+                         6) Get the total price of the items in the cart\s
+                         7) Get empty cart
+                         8) Get the average amount spent""");
 
 
-            String operCarr = sc.nextLine();
-            switch (operCarr) {
-                case "1"://controllo stato carrello
-                    if(stampCart(cart)){
-                        System.out.println("The cart is empty");
-                    }else{
-                        System.out.println(cart);
-                    }
-                    break;
-                case "2"://aggiunta elementi da carrello tramite id
-                    addId(arrayTemp, cart);
-                    break;
-                case "3"://rimozione elementi da carrello tramite id
-                    insertRemoveId(cart, arrayTemp);
-                    break;
-                case "4"://Finalizza acquisti
-                    buyProducts(cart, stock, arrayTemp);
-                    break;
-                case "5"://Aggiunta prodotti al carrello
-                    insertAddProdCart(cart, arrayTemp);
-                    break;
-                case "6"://Prezzo totale dei prodotti nel carrello.
-                    cartTotal(cart);
-                    break;
-                case "7":
-                    cart.getCart().clear();
-                    break;
-                case "8":
-                    averageSpending(cart);
-                    break;
+                String operCarr = sc.nextLine();
+                switch (operCarr) {
+                    case "1"://controllo stato carrello
+                        if (stampCart(cart)) {
+                            System.out.println("The cart is empty");
+                        } else {
+                            System.out.println(cart);
+                        }
+                        stay3 = false;
+                        break;
+                    case "2"://aggiunta elementi da carrello tramite id
+                        addId(arrayTemp, cart);
+                        stay3 = false;
+                        break;
+                    case "3"://rimozione elementi da carrello tramite id
+                        insertRemoveId(cart, arrayTemp);
+                        stay3 = false;
+                        break;
+                    case "4"://Finalizza acquisti
+                        buyProducts(cart, stock, arrayTemp);
+                        stay3 = false;
+                        break;
+                    case "5"://Aggiunta prodotti al carrello
+                        insertAddProdCart(cart, arrayTemp);
+                        stay3 = false;
+                        break;
+                    case "6"://Prezzo totale dei prodotti nel carrello.
+                        cartTotal(cart);
+                        stay3 = false;
+                        break;
+                    case "7":
+                        cart.getCart().clear();
+                        stay3 = false;
+                        break;
+                    case "8":
+                        averageSpending(cart);
+                        stay3 = false;
+                        break;
+                    default:
+                        System.out.println("Invalid input.");
+                        stay3 = true;
+                }
             }
             boolean stay2 = true;
             while (stay2) {
